f760a5111dfa10cb4ec78ef67cf2f3d17c199636
nzumbi <felixnjema@gmail.com>
2022-12-08 01:33:49 -0800

    formated code in 4 to betty styling

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
-#include <stdio.h>
 
+
+#include <stdio.h>
+/**
+ *  * main
+ *   *
+ *    * Return: 0
+ *     */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
