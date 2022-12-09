741a3330d97d5be97f40009ef523f98d652b3c5f
nzumbi <felixnjema@gmail.com>
2022-12-09 15:46:38 +0300

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+
+
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Print numbers to base 10,from 0.
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	putchar('\n');
+
+	return (0);
+}
