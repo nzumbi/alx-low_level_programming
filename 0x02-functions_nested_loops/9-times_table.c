9a8a04eb11e01da4b6be27aae188f410c6dacb19
nzumbi <felixnjema@gmail.com>
2022-12-13 22:19:15 +0300

    edit task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,34 +4,33 @@
  * times_table :prints the 9 times table
 **/
 
-void times_table(void)
+void time_table(void)
 {
-	int a, b, c;
-
-	for (a = 0; a <= 9; a++)
-	{
-
-		for (b = 0; b <= 9; b++)
-		{
-			c = a * b;
-
-			if (c <= 9)
-			{
-				_putchar(' ');
-			}
-			else
-			{
-				_putchar((c / 10) + '0');
-			}
-			_putchar((c % 10) + '0');
-
-			if (b != 9)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-		}
-
-		_putchar('\n');
-	}
+int a, b, c;
+for (a = 0; a < 10; a++)
+{
+for (b = 0; b < 10; b++)
+{
+c = b * a;
+if (b == 0)
+{
+_putchar(c + '0');
+}
+if (c < 10 && b != 0)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(c + '0');
+}
+else if (c >= 10)
+{
+_putchar(',');
+_putchar(' ');
+_putchar((c / 10) + '0');
+_putchar((c % 10) + '0');
+}
+}
+_putchar('\n');
+}
 }
