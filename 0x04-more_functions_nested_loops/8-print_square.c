1a2225abfe63d8f158809559546bd87b2639d1ff
nzumbi <felixnjema@gmail.com>
2022-12-15 14:53:34 +0300

    task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*print_square - prints squares in terminal
+*@size: parameter to be used
+*Return: nothing
+*/
+
+void print_square(int size)
+{
+	int x, y;
+
+	if (size > 0)
+	{
+		for (x = 0; x < size; x++)
+		{
+			for (y = 0; y < (size - 1); y++)
+			{
+				putchar('#');
+			}
+
+			putchar('#');
+			putchar('\n');
+		}
+	}
+	else
+	{
+		putchar('\n');
+	}
+}
