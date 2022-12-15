cd05f61c2faf91efe23182f93e58d3f76e84c030
nzumbi <felixnjema@gmail.com>
2022-12-15 17:17:41 +0300

    task 10 changes

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -14,16 +14,16 @@ void print_triangle(int size)
 		_putchar('\n');
 	} else
 	{
-		int i, j;
+		int a, b;
 
-		for (i = 1; i <= size; i++)
+		for (a = 1; a <= size; a++)
 		{
-			for (j = i; j < size; j++)
+			for (b = 1; b < size - 1; b++)
 			{
 				_putchar(' ');
 			}
 
-			for (j = 1; j <= i; j++)
+			for (b = 1; b <= a; b++)
 			{
 				_putchar('#');
 			}
