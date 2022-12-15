fa08440b17ccf2505501363e0069bba4147fced7
nzumbi <felixnjema@gmail.com>
2022-12-15 16:55:49 +0300

    fix 8 to task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -16,11 +16,6 @@ void print_triangle(int size)
 
 		for (a = 1; a <= size; a++)
 		{
-			for (b = a; b < size; b++)
-			{
-				_putchar(' ');
-			}
-
 			for (b = 1; b <= a; b++)
 			{
 				_putchar('#');
