b8bc3373ce559de43e718f2d3925bc5813d083d2
nzumbi <felixnjema@gmail.com>
2022-12-15 14:26:33 +0300

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*print_numbers - prints all numbers from
+*0 to 9
+*
+*Return: nothing
+*/
+
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar(10);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -10,5 +10,6 @@
 int _isupper(int);
 int _isdigit(int c);
 int mul(int a, int b);
+void print_numbers(void);
 
 #endif
