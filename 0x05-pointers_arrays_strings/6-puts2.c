e62c49073c15917b01193c4ed3a3e0e9a69aea91
nzumbi <felixnjema@gmail.com>
2022-12-19 13:33:38 +0300

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every caracter of a string
+ * followed by a new line
+ * @str: input string.
+ */
+void puts2(char *str)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		if (str[count] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		if (count % 2 == 0)
+			_putchar(str[count]);
+		count++;
+	}
+}
