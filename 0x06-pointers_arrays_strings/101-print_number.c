1213e5e273ca28ba3d1b64f7c29065f410a66ef0
nzumbi <felixnjema@gmail.com>
2022-12-21 13:57:31 +0300

    task 9

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_number - function prints integer;
+ * @n: integer
+ */
+void print_number(int n)
+{
+	unsigned int a;
+
+	if (n < 0)
+	{
+		a = -n;
+		_putchar('-');
+	} else
+	{
+		a = n;
+	}
+
+	if (a / 10)
+		print_number(a / 10);
+
+	_putchar((a % 10) + '0');
+}
